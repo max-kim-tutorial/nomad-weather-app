a103b22ea26c0c54a8b20dfc4e37c26a4d3b8744
Max Kim <hwaseen@gmail.com>
2020-06-01 11:05:19 +0900

    Loading Component

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import React from "react";
 // 브릿지 - 자바스크립트의 폰의 코드 소통
 import { StyleSheet, Text, View, TouchableWithoutFeedback } from "react-native";
-
+import Loading from "./Loading";
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.yellowView}>
-        <Text>안뇽</Text>
-      </View>
-      <View style={styles.blueView}>
-        <Text>안뇽2</Text>
-      </View>
+      <Loading />
     </View>
   );
 }
@@ -18,13 +13,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor: "yellow",
-  },
-  blueView: {
-    flex: 1,
-    backgroundColor: "blue",
   },
 });
