3d5293d701bb2f991ebda4d257e7e27238778f67
Max Kim <hwaseen@gmail.com>
2020-06-01 11:16:21 +0900

    Appying location api

--- App.js
+++ App.js
@@ -2,12 +2,23 @@ import React from "react";
 // 브릿지 - 자바스크립트의 폰의 코드 소통
 import { StyleSheet, Text, View, TouchableWithoutFeedback } from "react-native";
 import Loading from "./Loading";
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Loading />
-    </View>
-  );
+import * as Location from "expo-location";
+
+export default class App extends React.Component {
+  getLocation = async () => {
+    const location = await Location.getCurrentPositionAsync();
+    console.log(location);
+  };
+  componentDidMound() {
+    this.getLocation();
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Loading />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
