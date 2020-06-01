191684d9b7a7ab25bde0ebe63b0bf9473de245dd
Max Kim <hwaseen@gmail.com>
2020-06-01 12:19:03 +0900

    Request weather info with openweatherAPI

--- App.js
+++ App.js
@@ -3,10 +3,18 @@ import React from "react";
 import { StyleSheet, Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
 
+const API_KEY = "958d007ab9ed74bdebc834b9e5756819";
 export default class extends React.Component {
   state = {
     isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
   };
   getLocation = async () => {
     try {
@@ -16,6 +24,7 @@ export default class extends React.Component {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
       console.log(latitude, longitude);
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("위치를 찾을 수 없습니다.", "ㅜㅜ");
