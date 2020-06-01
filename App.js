ea9dff045148965f6587f3d1c83a9ddc227698c7
Max Kim <hwaseen@gmail.com>
2020-06-01 10:51:56 +0900

    element 소개

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+// 브릿지 - 자바스크립트의 폰의 코드 소통
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>안뇽!</Text>
     </View>
   );
 }
@@ -12,8 +13,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "skyblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
   },
 });
---
+++ README.md
@@ -0,0 +1,13 @@
+# RN Tutorial
+
+## 브릿지
+- 자바스크립트와 모바일 환경의 소통
+- 자바스크립트 <-> 모바일의 네이티브 텍스트(swift, java)
+
+### 종류
+- html 태그를 쓰지는 못한다
+- View : \<div>, 모든게 view
+- Text : \<span>
+- CSS는 -를 빼고 StyleSheet api 사용하여 생성
+- 모든것들이 호환되지는 않음
+
