09b59eee33b60e1793bc275781aba814e3e94dac
Max Kim <hwaseen@gmail.com>
2020-06-01 11:34:55 +0900

    Applying permission api

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
 import React from "react";
 // 브릿지 - 자바스크립트의 폰의 코드 소통
-import { StyleSheet, Text, View, TouchableWithoutFeedback } from "react-native";
+import { StyleSheet, Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 
-export default class App extends React.Component {
-  getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+export default class extends React.Component {
+  state = {
+    isLoading: true,
   };
-  componentDidMound() {
+  getLocation = async () => {
+    try {
+      // 프로미스를 반환하고 허가를 안주면 에러를 발생시킴
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      console.log(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("위치를 찾을 수 없습니다.", "ㅜㅜ");
+    }
+  };
+  componentDidMount() {
     this.getLocation();
   }
   render() {
-    return (
-      <View style={styles.container}>
-        <Loading />
-      </View>
-    );
+    const { isLoading } = this.state;
+
+    return isLoading ? <Loading /> : null;
   }
 }
 
